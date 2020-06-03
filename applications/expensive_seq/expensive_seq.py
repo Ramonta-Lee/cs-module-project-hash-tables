cc46b7cd8b42b20a6128bfefd085cdd4f9609b96
unknown <viola4lfe@gmail.com>
2020-06-03 17:36:39 -0400

    expensive_seq complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,23 @@
 # Your code here
 
+def expensive_seq(x, y, z, cache={}):
+    
+    # follow example function
+    if x <= 0:
+        return y + z
+    if x > 0:
+        # using method from lookup, check to see if value (tuple this time) has
+        # been added to cache
+        if (x, y, z) not in cache:
+            # if not add it cache with it's math
+            cache[(x, y, z)] = expensive_seq(x - 1, y + 1, z) + \
+                expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
+            return cache[(x, y, z)] # used in the recursive calling
+        return cache[(x, y, z)]
+        # return the value from cache
 
-def expensive_seq(x, y, z):
-    # Your code here
+   
+
 
 
 
