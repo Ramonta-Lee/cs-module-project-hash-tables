b7218b6958d598f42742b7e64fd30e159da28895
unknown <viola4lfe@gmail.com>
2020-06-03 21:24:49 -0400

    completed no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,23 @@
 def no_dups(s):
     # Your code here
+    # return ' '.join(set(s.split())) # doesn't account for order
 
-
+    words = []
+    spaces = ' '
+    length = len(s)
+    i = 0
+    while i < length:
+        if s[i] not in spaces:
+            word_start = i
+            while i < length and s[i] not in spaces:
+                i += 1
+            words.append(s[word_start:i])
+        i += 1
+    words_stack = []
+    for val in words:  #
+        if val not in words_stack:  # We can replace these three lines with this one -> [words_stack.append(val) for val in words if val not in words_stack]
+            words_stack.append(val)  #
+    return " ".join(words_stack)
 
 if __name__ == "__main__":
     print(no_dups(""))
--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,29 @@
+counts = {}
 def word_count(s):
     # Your code here
+    
+    ignore = ' : ; , . - + = / \ | [ ] { } ( ) * ^ & " '
+    for x in s:
+        if x in ignore:
+            s = s.replace(x, " ")
+
+    # joins the words together removes extra space then splits by a space
+    s = " ".join(s.split())
+    # print("s", s)
+
+    # puts words in a list of strings separated by a comma
+    new_s = s.split()
+    # print("new", new_s)
+
+    for c in new_s:
+        c = c.lower()  # case insensitive
+        if c in counts:
+            counts[c] += 1
+        else:
+            counts[c] = 1
+
+    return counts
+        
 
 
 
